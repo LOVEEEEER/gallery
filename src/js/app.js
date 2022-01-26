e82281ff8e1fce8d9309496400dffb2be1244699
Nikita <92940398+LOVEEEEER@users.noreply.github.com>
2022-01-27 01:35:24 +1000

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+
+        slide.classList.toggle('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
